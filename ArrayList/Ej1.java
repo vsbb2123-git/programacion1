29c82a03028f96a59463ea63df95ddb870c633b7
pokE02 <pokE02@x.y.z>
2023-05-05 12:30:07 +0200

    e1

---
+++ ArrayList/Ej1.java
@@ -0,0 +1,21 @@
+package ArrayList;
+
+import java.util.ArrayList;
+
+public class Ej1 {
+    public static void menuPrincipal() 
+    {
+        ArrayList<String> colores=new ArrayList <String>();
+        colores.add("Rojo");
+        colores.add("Azul");
+        colores.add("Verde");
+        colores.add("Naranja");
+        colores.add("Blanco");
+        System.out.println();///se puede sacar por pantalla con un sout
+        for(int i=0;i<colores.size();i++)
+        {
+            System.out.println("Pos: "+i+" Color: "+colores.get(i));
+        }
+    }
+    
+}
