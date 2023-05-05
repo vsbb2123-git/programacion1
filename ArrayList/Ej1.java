01c49291affcd02c85dcd32906a47c24811071e6
pokE02 <pokE02@x.y.z>
2023-05-05 12:34:17 +0200

    l

--- ArrayList/Ej1.java
+++ ArrayList/Ej1.java
@@ -3,7 +3,7 @@ package ArrayList;
 import java.util.ArrayList;
 
 public class Ej1 {
-    public static void menuPrincipal() 
+    public static void main(String args[])
     {
         ArrayList<String> colores=new ArrayList <String>();
         colores.add("Rojo");
