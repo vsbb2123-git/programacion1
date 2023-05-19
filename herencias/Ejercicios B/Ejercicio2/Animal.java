566b46cc168967845af25abbaffed5572e254952
vsbb2123-insti <vsantamaria2123@gmail.com>
2023-05-19 13:39:56 +0200

    Ejercicios 1 y dos del B

---
+++ herencias/Ejercicios B/Ejercicio2/Animal.java
@@ -0,0 +1,13 @@
+public class Animal 
+{
+    String texto;
+
+    protected Animal()
+    {
+        texto="Animal";
+    }
+    public String toString()
+    {
+        return texto+" "; 
+    }    
+}
---
+++ herencias/Ejercicios B/Ejercicio2/MiMundo.java
@@ -0,0 +1,7 @@
+import java.util.ArrayList;
+
+public class MiMundo {
+    public static void main(String[] args) {
+    
+    }
+}
